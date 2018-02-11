28eb98da4276491c81ab43b476270b1415c6982e
Monica Siegel <mhsiegel@umich.edu>
2018-02-11 15:24:01 -0500

    added tweets

--- hw5_twitter.py
+++ hw5_twitter.py
@@ -4,12 +4,11 @@ import sys
 import requests
 import secret_data # file that contains OAuth credentials
 import nltk
-# import nltk # uncomment line after you install nltk
 
 ## SI 206 - HW
 ## COMMENT WITH:
 ## Your section day/time: 002/2 pm
-## Any names of people you worked with on this assignment: Tori Engler
+## Any names of people you worked with on this assignment: Tori Engler, Michelle Phillips
 
 #usage should be python3 hw5_twitter.py <username> <num_tweets>
 username = sys.argv[1]
@@ -26,15 +25,41 @@ auth = OAuth1(consumer_key, consumer_secret, access_token, access_secret)
 requests.get(url, auth=auth)
 #Code for OAuth ends
 
-#Write your code below:
-#Code for Part 3:Caching
-#Finish parts 1 and 2 and then come back to this
+CACHE_FNAME = 'twitter_cache.json'
+try:
+    cache_file = open(CACHE_FNAME, 'r')
+    cache_contents = cache_file.read()
+    CACHE_DICTION = json.loads(cache_contents)
+    cache_file.close()
+except:
+    CACHE_DICTION = {}
+data = open(CACHE_FNAME, 'w')
 
-#Code for Part 1:Get Tweets
-
-#Code for Part 2:Analyze Tweets
-
-
+def get_tweets(username, num_tweets):
+    user_url = 'https://api.twitter.com/1.1/search/tweets.json?q={}&count={}'.format(username, num_tweets)
+    if user_url in CACHE_DICTION:
+        print('checking cache...')
+        json_data = CACHE_DICTION[user_url]
+    else:
+        print('fetching data...')
+        request = requests.get(user_url, auth = auth)
+        json_data = json.loads(request.text)
+        write_file = open('tweet.json', 'w')
+        write_file.write(json.dumps(json_data, indent = 4))
+        write_file.close()
+        write_file = open(CACHE_FNAME, 'w')
+        CACHE_DICTION[user_url] = json_data
+        write_file.write(json.dumps(CACHE_DICTION))
+        write_file.close()
+    tweets = ''
+    for x in json_data['statuses']:
+        tweets += x['text']
+    tokens = nltk.word_tokenize(tweets)
+    freqDist = nltk.FreqDist(token for token in tokens if token.isalpha() and "https" not in token and 'http' not in token and 'RT' not in token)
+    for word, frequency in freqDist.most_common(5):
+        print(word + " " + str(frequency))
+    return CACHE_DICTION[user_url]
+get_tweets(username, num_tweets)
 
 if __name__ == "__main__":
     if not consumer_key or not consumer_secret:
